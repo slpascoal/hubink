872c7d4bfceb6e6bfcab1a35955cf693fa44b08a
Silas Pascoal <silas.pascoal@onfly.com.br>
2025-02-27 11:34:25 -0300

    instalando daisyUI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
 <!doctype html>
-<html lang="{{config('app.locale')}}">
+<html lang="{{config('app.locale')}}" class="h-full">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hubink</title>
     @vite('resources/css/app.css')
 </head>
-<body class="bg-slate-900 text-slate-50">
+<body class="bg-red-900 text-slate-50 h-full">
     <h1 class="font-bold text-3xl">Hello World!!</h1>
 </body>
 </html>
